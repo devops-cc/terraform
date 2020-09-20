fdea94af7bda02558eeb1f0db96c77c18b2b3be4
tharun <tharun.reddy@cerner.com>
2020-09-20 18:46:33 -0400

    updated config

--- terraform_aws/aws_1.tf
+++ terraform_aws/aws_1.tf
@@ -7,41 +7,120 @@ terraform {
   }
 }
 
+# aws access keys
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
-variable  "aws_access_key" { }
-variable  "aws_secret_key" { }
 
-variable  "aws_region" { 
-    default = "us-east-1"
+# key name refers to a key pair that exits with in aws, so that we can ssh into the instance once its created. 
+variable "key_name" {}
+# path to the private key that corresponds to the key pair thats in aws.
+variable "private_key_path" {}
+
+# variable to aws region
+variable "aws_region" {
+  default = "us-east-1"
 }
-variable  "aws_instance_type" { 
-    default = "t2.micro"
+
+# variable to define tyoe of instance
+variable "aws_instance_type" {
+  default = "t2.micro"
 }
 
 provider "aws" {
-  profile = "default"
-  aws_access_key= "var.aws_access_key"
-  aws_secret_key = "var.aws_secret_key"
-  region  = "var.aws_region"
+  profile        = "default"
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  region         = var.aws_region
 }
 
-# get ec2 data source from the provider
 
-data "aws_ami" "alx"{
-    most_recent = true
-    owners = ["amazon"]
-    filters{}
+# DATA section
+
+# pulling the data from the provider
+# we need get the latest aws linux ami for the ec2 instace. so we need to get the latest version 
+# of AMI image. below 
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filters {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+  filters {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filters {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-resource "aws_instance" "example" {
-  ami           = "data.aws_ami.alx.id"
-  instance_type = "var.aws_instance_type"
+
+# resources section
+
+
+# this uses the default vpc. It will not delete it or destroy
+resource "aws_default_vpc" "default" {
 }
 
-output "aws_public_ip"{
-    value = "aws_instance.ex.public_dns"
+resource "aws_security_group" "allow_ssh" {
+  name        = "nginx_demo"
+  description = "allow ports for nginx demo"
+  vpc_id      = aws_default_vpc.default.id
+  # ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # nginix web server
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # traffic to go the internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
- 
+
+resource "aws_instance" "nginix" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "var.aws_instance_type"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+
+  }
+}
+
+# output
+output "aws_instance_public_dns" {
+  value = aws_instance.nginix.public_dns
+}
+
 # Create a VPC
 # resource "aws_vpc" "example" {
 #   cidr_block = "10.0.0.0/16"
---
+++ terraform_aws/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_access_key   = ""
+aws_secret_key   = ""
+key_name         = "aws_one_key"
+private_key_path = "/Users/tc045537/gitRepo/terraform/private_keys/aws_one_keyaws_one_key.pem"
